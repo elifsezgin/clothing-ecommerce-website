ae4ffec62fa1ed5c514de698069f492924a13980
elifsezgin <elifmsezgin@gmail.com>
2024-07-19 10:52:33 +0300

    Quick fix

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import {ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import CartIcon from '../../components/cart-icon/CartIcon'
